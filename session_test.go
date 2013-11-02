6a5418ca899c7e86177d2f718a5225a3e1901ee0
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-11-03 06:21:10 +0800

    Move unrelated code away from session_test.go

--- session_test.go
+++ session_test.go
@@ -2,32 +2,9 @@ package basecrm
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
-	"sync"
 	"testing"
 )
-
-type fakeTransport struct {
-	Answer     func(*http.Request) string
-	StatusCode int
-	requests   []*http.Request
-	sync.Mutex
-}
-
-func (ft *fakeTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	ft.Lock()
-	ft.requests = append(ft.requests, req)
-	ft.Unlock()
-	res := &http.Response{
-		Status:     fmt.Sprintf("%d %s", ft.StatusCode, http.StatusText(ft.StatusCode)),
-		StatusCode: ft.StatusCode,
-		Header:     make(http.Header),
-		Body:       ioutil.NopCloser(strings.NewReader(ft.Answer(req))),
-	}
-	return res, nil
-}
 
 func TestAuthenticationSuccess(t *testing.T) {
 	ft := &fakeTransport{
